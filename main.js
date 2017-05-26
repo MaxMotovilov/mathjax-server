40cefb3739f3d59686b6df3ee97fd050c821b6c5
Michael van der Werve <mvdwerve@outlook.com>
2017-05-26 17:29:08 +0200

    some changes to have a nice UI

--- main.js
+++ main.js
@@ -1,45 +1,53 @@
 // we need the http server and are going to set the port at a default 3000
-const http = require('http')
-const port = 3000
+var express = require('express')
+var fs = require('fs');
 
 // a simple TeX-input example
 var mathjax = require("mathjax-node-svg2png");
 mathjax.config({
-    MathJax: {}
+  MathJax: {}
 });
 
 // start the mathjax api
 mathjax.start();
 
-// make the request handler
-const requestHandler = (request, response) => {
-    // remove starting /
-    url = request.url.substr(1);
+// load the index.html file into memory
+page = fs.readFileSync('index.html', 'utf8');
 
-    // we need to decode the url
-    math = decodeURI(url);
+// we start the app
+var app = express();
 
-    // typeset it 
-    mathjax.typeset({
-        math: math,
-        format: "AsciiMath", // we always use AsciiMath, because we are limited
-        png: true, //  we create a png
-        scale: 2
-    }, function (data) {
-        console.log(data.errors)
-        console.log(math)
-        // we always assume it goes correctly for now
-        base64 = data.png.substr(22)
-        png = new Buffer(base64, 'base64')
-        response.setHeader('Content-Type', 'image/png');
-        response.end(png);
-    });
-}
+// we want the basic, nothin version for the html
+app.get('/', function(req, res) {
+  res.send(page);
+});
 
-// make the server
-const server = http.createServer(requestHandler)
+// we want the basic, nothin version for the html
+app.get('/render', function(req, res) {
+  // we need to decode the url
+  math = decodeURIComponent(req.query.tex);
+  scale = parseFloat(decodeURIComponent(req.query.scale));
 
-// now listen on the server
-server.listen(port, (err) => {
-    if (err) return console.log('We cannot start up for some reason...', err)
-})
+  // done if no length
+  if (math.length == 0) return;
+
+  // if the scale is undefined, we set it
+  if (req.query.scale === undefined) scale = 1;
+
+  // typeset it 
+  mathjax.typeset({
+    math: math,
+    format: "TeX", // we always use TeX (I love it)
+    png: true, //  we create a png
+    scale: scale
+  }, function(data) {
+    // we always assume it goes correctly for now
+    base64 = data.png.substr(22)
+    png = new Buffer(base64, 'base64')
+    res.setHeader('Content-Type', 'image/png');
+    res.end(png);
+  });
+});
+
+// let the app listen
+app.listen(5000);
